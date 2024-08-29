b4376e6f37b7008ec0e41c314fbd67f8e456a720
Yan Naing Aung <yannaingaung2209@gmail.com>
2024-08-29 10:54:11 +0630

    chore :added action to control using javascript

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello world...");
+//DOM Elements
+const boxes = document.querySelectorAll(".box");
+const image = document.getElementsByClassName("image")[0];
+
+//drag and drop function
+boxes.forEach((box) => {
+  box.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    box.classList.add("hovered");
+  });
+
+  box.addEventListener("dragleave", (event) => {
+    box.classList.remove("hovered");
+  });
+
+  box.addEventListener("drop", (event) => {
+    box.append(image); //move image tag to the new box
+    box.classList.remove("hovered");
+  });
+});
